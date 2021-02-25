0015b8eefce7a7e194b3317577d72c42bf39a39a
Joao Marcos R de Carvalho <jc808853@hpeautos.com.br>
2021-02-24 21:25:44 -0300

    add first resquest mapping to response

---
+++ example03_springmvc-sample/src/main/java/com/jodev/spring/springmvc/controllers/Controller01.java
@@ -0,0 +1,14 @@
+package com.jodev.spring.springmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Controller01 {
+	@ResponseBody //write the method return directly on the html response
+	@RequestMapping("/reports")
+	public String getReport() {
+		return "those are all the reports"; 
+	}
+}
