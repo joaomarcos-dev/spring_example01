dd1c44eb3a79e3481d1ba86065bddb6c268fbf0b
Joao Marcos R de Carvalho <jc808853@hpeautos.com.br>
2021-02-24 23:16:35 -0300

    add InternalResourceViewResolver test case and method that return view

--- example03_springmvc-sample/src/main/java/com/jodev/spring/springmvc/controllers/Controller02.java
+++ example03_springmvc-sample/src/main/java/com/jodev/spring/springmvc/controllers/Controller02.java
@@ -5,17 +5,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class Controller01 {
+@RequestMapping("/statistics")
+public class Controller02 {
 	@ResponseBody //write the method return directly on the html response
 	@RequestMapping("/reports")
 	public String getReport() {
-		return "those are all the reports"; 
+		return "those are all the reports UNDER statistics"; 
 	}
 	
 	@ResponseBody //write the method return directly on the html response
 	@RequestMapping("/kpis")
 	public String getKpi() {
-		return "those are all the KPI's"; 
+		return "those are all the KPI's UNDER statistics"; 
+	}
+	
+//	@ResponseBody //write the method return directly on the html response
+	@RequestMapping("/dashboard")
+	public String getDashboard() {
+		return "/WEB-INF/view/View01.jsp"; 
+	}
+	
+	@RequestMapping("/dashboardVR")
+	public String getDashboardViewResolver() {
+		
+		return "View01"; 
 	}
 }
 
